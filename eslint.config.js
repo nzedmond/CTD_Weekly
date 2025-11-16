a181d4f9ccfddcd338840ae45d5df79a009e3ad0
Edmond <enzivugira@colgate.edu>
2025-11-15 20:58:56 -0500

    Finished Assignment 01

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -15,6 +16,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
+      setting: {react: {version: 'detect'}},
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -24,6 +26,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-used-vars': 'warn', // this changes the error to a warning
+      'react/prop-types': 'off', //this suppresses warnings about not using prop-types.
     },
   },
 ])
